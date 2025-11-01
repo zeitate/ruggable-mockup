2691df9d7e628cd38c642ba536c393989c588f04
zeitate <anonimnotoriu@protonmail.com>
2025-11-01 20:26:50 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/ruggable-mockup/'  // 🔴 Must match your repo name exactly
+})
